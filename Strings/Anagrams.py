46b22bde4e66461c4a2ac11eb28d3e6fb32110e3
Devi Sri <149393942+DeviSri184@users.noreply.github.com>
2025-02-24 22:09:05 +0530

    Update Anagrams.py

--- Strings/Anagrams.py
+++ Strings/Anagrams.py
@@ -3,3 +3,19 @@ def are_anagrams(str1, str2):
 
 print(are_anagrams("Astronomer", "Moon starer"))
 
+
+"""Another Approach"""
+
+def isAnagram(s, t):
+    if len(s) != len(t):
+        return False
+    hash_map = defaultdict(int)
+
+    for i in s:
+        hash_map[i] += 1
+    for i in t:
+        if hash_map[i] == 0:
+            return False
+        hash_map[i] -= 1
+    return True
+
